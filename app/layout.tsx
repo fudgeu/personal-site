f85bc19b3e434139e1359adf1d5cdeeab48f4abe
Fudgeu <patrickzambonie@gmail.com>
2023-05-08 23:54:58 -0400

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Source_Code_Pro } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const sourceCodePro = Source_Code_Pro({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={sourceCodePro.className}>{children}</body>
     </html>
   )
 }
